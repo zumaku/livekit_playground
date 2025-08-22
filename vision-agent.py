38bbd2979b52851024adba617ee86203ea2423e0
Zul Fadli <60200121101@uin-alauddin.ac.id>
2025-08-22 10:29:36 +0800

    vision agent

---
+++ vision-agent.py
@@ -0,0 +1,85 @@
+import logging
+import asyncio
+import base64
+from dotenv import load_dotenv
+
+from livekit.agents import (
+    Agent,
+    AgentSession,
+    JobContext,
+    RoomInputOptions,
+    WorkerOptions,
+    cli,
+    get_job_context,
+)
+from livekit.agents.llm import ImageContent
+from livekit.plugins import google, noise_cancellation
+
+logger = logging.getLogger("vision-assistant")
+
+load_dotenv()
+
+
+class VisionAssistant(Agent):
+    def __init__(self) -> None:
+        self._tasks = []
+        super().__init__(
+            instructions="Kamu adalah AMI, seorang voice assistant yang baik.",
+            llm=google.beta.realtime.RealtimeModel(
+                voice="Puck",
+                temperature=0.8,
+            ),
+        )
+
+    async def on_enter(self):
+        def _image_received_handler(reader, participant_identity):
+            task = asyncio.create_task(
+                self._image_received(reader, participant_identity)
+            )
+            self._tasks.append(task)
+            task.add_done_callback(lambda t: self._tasks.remove(t))
+            
+        get_job_context().room.register_byte_stream_handler("test", _image_received_handler)
+
+        self.session.generate_reply(
+            instructions="Sapa pengguna secara singkat dan tawarkan bantuan Anda."
+        )
+    
+    async def _image_received(self, reader, participant_identity):
+        logger.info("Received image from %s: '%s'", participant_identity, reader.info.name)
+        try:
+            image_bytes = bytes()
+            async for chunk in reader:
+                image_bytes += chunk
+
+            chat_ctx = self.chat_ctx.copy()
+            chat_ctx.add_message(
+                role="user",
+                content=[
+                    ImageContent(
+                        image=f"data:image/png;base64,{base64.b64encode(image_bytes).decode('utf-8')}"
+                    )
+                ],
+            )
+            await self.update_chat_ctx(chat_ctx)
+            print("Image received", self.chat_ctx.copy().to_dict(exclude_image=False))
+        except Exception as e:
+            logger.error("Error processing image: %s", e)
+
+
+async def entrypoint(ctx: JobContext):
+    await ctx.connect()
+    
+    session = AgentSession()
+    await session.start(
+        agent=VisionAssistant(),
+        room=ctx.room,
+        room_input_options=RoomInputOptions(
+            video_enabled=True,
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
